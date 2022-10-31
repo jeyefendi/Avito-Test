6d29f15e8e15a2ba0a6242c2ed6ca93c3456df98
Jeyhun Farajov <97691925+jeyefendi@users.noreply.github.com>
2022-10-31 18:50:42 +0300

    infinite scroll

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -2,10 +2,9 @@ import styles from './Feed.module.sass'
 import Story from "./Story";
 import React, {useEffect, useState} from "react";
 import { getStoryIds } from '../../services/api';
-import { useInfiniteScroll } from '../../services/useInfiniteScroll';
 
 const Feed = () => {
-  const { count } = useInfiniteScroll();
+
   const [storyIds, setStoryIds] = useState([]);
 
   useEffect(() => {
@@ -13,11 +12,9 @@ const Feed = () => {
   }, []);
 
 
-  return (
+  return storyIds.map(storyId =>
     <main className={styles.feed}>
-      {storyIds.slice(0, count).map(storyId => (
       <Story key={storyId} storyId={storyId} />
-      ))}
     </main>
   )
 }
