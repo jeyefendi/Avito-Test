403034e744e366b5a44aca2de0a22cb54f1d5c0c
Jeyhun Farajov <97691925+jeyefendi@users.noreply.github.com>
2022-11-01 22:02:59 +0300

    refetch button

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -4,30 +4,38 @@ import { useQuery } from "react-query";
 import { getStoryIds } from "../../services/api";
 
 const Feed = () => {
-
-  const { isLoading, data, isError, error } = useQuery('hacker-news', getStoryIds,
+  const { isLoading, data, isError, error, refetch } = useQuery(
+    "hacker-news",
+    getStoryIds,
     {
       staleTime: 60000,
       refetchInterval: 60000,
       refetchIntervalInBackground: true,
     }
-  )
+  );
 
   if (isLoading) {
-    return <h2 className="mx-16 my-8">Loading...</h2>
+    return <h2 className="mx-16 my-8">Loading...</h2>;
   }
 
   if (isError) {
-    return <h2 className="mx-16 my-8">{error.message}</h2>
+    return <h2 className="mx-16 my-8">{error.message}</h2>;
   }
 
   return (
-    <main className={styles.feed}>
-      {data.slice(0, 100).map(storyId => (
-        <Story key={storyId} storyId={storyId} />
-      ))}
-    </main>
-  )
-}
+    <>
+      <main className={styles.feed}>
+        {data.slice(0, 100).map((storyId) => (
+          <>
+            <Story key={storyId} storyId={storyId} />
+          </>
+        ))}
+      </main>
+      <button onClick={refetch} className="absolute top-4 right-16">
+        <img src="/refresh.png" alt="Refetch" className="w-16"/>
+      </button>
+    </>
+  );
+};
 
 export default Feed;
